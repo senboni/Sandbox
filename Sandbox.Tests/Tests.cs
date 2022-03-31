61f7a491bb0d9ab47ab7325ea6afe3319cc0c27b
Senboni <kajler11@gmail.com>
2022-03-31 16:57:30 +0200

    initial commit

---
+++ Sandbox.Tests/Tests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace Sandbox.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.True(false);
+    }
+}
